f55e420f5cd4e84fa4afdb1bb075d1fbd7311175
“Avital2025” <A0583273344@gmail.com>
2024-12-03 17:22:39 +0200

    load 3.1

--- Queue_management_system/Queue_management_system.Api/Program.cs
+++ Queue_management_system/Queue_management_system.Api/Program.cs
@@ -1,49 +1,53 @@
-using Queue_management_system;
 using Queue_management_system.Core.IRepository;
 using Queue_management_system.Core.Iservice;
 using Queue_management_system.Data.Repository;
+using Queue_management_system.Service.Entities;
 using Queue_management_system.service;
+using Queue_management_system;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddScoped<IEmployeesService,EmployeesService>();
-builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
-/*
- לטפל להחליף--------------------
-builder.Services.AddScoped<IPatientsService, PatientsService>();
-builder.Services.AddScoped<IPatientsRepository, PatientsRepository>();
-
-builder.Services.AddScoped<IEmployeesService, EmployeesService>();
-builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
-
-builder.Services.AddScoped<IEmployeesService, EmployeesService>();
-builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
-
-builder.Services.AddScoped<IEmployeesService, EmployeesService>();
-builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
-*/
-
-
-
-builder.Services.AddSingleton<DataContext>();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddScoped<IEmployeesService, EmployeesService>();
+        builder.Services.AddScoped<IGenericRepository<EmployeesEntity>, EmployeesRepository>();
+
+
+        builder.Services.AddScoped<IPatientsService, PatientsService>();
+        builder.Services.AddScoped<IGenericRepository<PatientsEntity>, PatientsRepository>();
+
+        builder.Services.AddScoped<IQueuesService, QueuesService>();
+        builder.Services.AddScoped<IGenericRepository<QueuesEntity>, QueuesRepository>();
+
+        builder.Services.AddScoped<IRoomsService, RoomsService>();
+        builder.Services.AddScoped<IGenericRepository<RoomsEntity>, RoomsRepository>();
+
+
+
+
+        builder.Services.AddSingleton<DataContext>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
